4c20fd81d692a09102fbe74f25e55f2c9f1fbb7b
Katniss Lee/LGEMR MC North America RKatniss.leeD Network Based Applications Team(katniss.lee@lge.com) <katniss.lee@lge.com>
2019-06-05 17:31:19 -0700

    June 5th update

--- app/src/main/java/com/example/memopad/WriteNewActivity.java
+++ app/src/main/java/com/example/memopad/WriteNewActivity.java
@@ -2,6 +2,8 @@ package com.example.memopad;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class WriteNewActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class WriteNewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_new);
+    }
+
+    public void onClickSave(View view) {
+        CharSequence text = "Your message has been saved.";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(this, text, duration);
+        toast.show();
     }
 }
