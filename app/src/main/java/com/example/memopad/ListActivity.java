f9fca1928da5b4457378c70fb470ce761b2be936
Katniss Lee/LGEMR MC North America RKatniss.leeD Network Based Applications Team(katniss.lee@lge.com) <katniss.lee@lge.com>
2019-06-12 16:39:45 -0700

    finished creating app bar

--- app/src/main/java/com/example/memopad/ListActivity.java
+++ app/src/main/java/com/example/memopad/ListActivity.java
@@ -1,8 +1,14 @@
 package com.example.memopad;
 
 import android.content.Intent;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.FloatingActionButton;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -21,6 +27,10 @@ public class ListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        //Set my toolbar and enable Up
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         //Set my listview with the received message
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
                 ListActivity.this, android.R.layout.simple_list_item_1, Message.messages);
@@ -32,7 +42,6 @@ public class ListActivity extends AppCompatActivity {
                     @Override
                     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                         String message = Message.messages.get(position);
-                        Message.messages.remove(position);
                         Intent intent = new Intent(ListActivity.this, WriteNewActivity.class);
                         intent.putExtra("message", message);
                         intent.putExtra("position", position);
@@ -40,7 +49,25 @@ public class ListActivity extends AppCompatActivity {
                     }
                 };
         listView.setOnItemClickListener(itemClickListener);
+}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.create_new_memo:
+                Intent intent = new Intent(ListActivity.this, WriteNewActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
+    }
 
     public void onClickAdd(View view) {
         Intent intent = new Intent(ListActivity.this, WriteNewActivity.class);
--- app/src/main/java/com/example/memopad/WriteNewActivity.java
+++ app/src/main/java/com/example/memopad/WriteNewActivity.java
@@ -3,11 +3,16 @@ package com.example.memopad;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.annotation.RequiresPermission;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.support.v7.widget.ShareActionProvider;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -17,12 +22,20 @@ import java.util.ArrayList;
  */
 public class WriteNewActivity extends AppCompatActivity {
 
+    private ShareActionProvider shareActionProvider;
     int TEXT_POSITION = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_new);
+        //Set up the same toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        //if we are opening up previous message, add that text to the screen
         if (getIntent().getExtras() != null) {
             Intent intent = getIntent();
             String message = intent.getStringExtra("message");
@@ -32,12 +45,46 @@ public class WriteNewActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_write, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void setShareActionIntent(String text) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.trash_memo:
+                alert();
+                return true;
+
+            case R.id.share_memo:
+                EditText editText = (EditText) findViewById(R.id.message);
+                String messageText = editText.getText().toString();
+                setShareActionIntent(messageText);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
     public void onClickSave(View view) {
         CharSequence text = "Your message has been saved.";
         int duration = Toast.LENGTH_SHORT;
         Toast toast = Toast.makeText(this, text, duration);
         toast.show();
 
+        if (TEXT_POSITION != -1) {
+            Message.messages.remove(TEXT_POSITION);
+        }
         EditText message = (EditText) findViewById(R.id.message);
         String messageText = message.getText().toString();
         Message.messages.add(0, messageText);
@@ -45,13 +92,10 @@ public class WriteNewActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void onClickShare(View view) {
-        EditText message2 = (EditText) findViewById(R.id.message);
-        String messageText2 = message2.getText().toString();
-        Intent shareIntent = new Intent(Intent.ACTION_SEND);
-        shareIntent.setType("text/plain");
-        shareIntent.putExtra("shared message", messageText2);
-        startActivity(shareIntent);
+    public String onClickShare() {
+        EditText message = (EditText) findViewById(R.id.message);
+        String messageText = message.getText().toString();
+        return messageText;
     }
 
     public void onClickDelete(View view) {
